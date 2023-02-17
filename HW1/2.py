f51ffa8cd22399d47137af21d094152090066d1b
telegins125@gmail.com <telegins125@gmail.com>
2023-02-17 17:45:10 +0300

    HW1

---
+++ HW1/2.py
@@ -0,0 +1,10 @@
+#входные данные
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+#важно чтобы знаки были одинаковыми
+if (x1*x2 > 0) and (y1*y2 > 0):
+    print('YES')
+else:
+    print('NO')
